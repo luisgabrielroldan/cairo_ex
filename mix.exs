8dfbbaf366664bd31230cac2553639993cf2b76b
Gabriel Roldan <luisgabrielroldan@gmail.com>
2020-09-21 17:58:12 -0300

    Prepare for release

--- mix.exs
+++ mix.exs
@@ -1,11 +1,17 @@
 defmodule CairoEx.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/luisgabrielroldan/cairo_ex"
+
   def project do
     [
       app: :cairo_ex,
-      version: "0.1.0",
+      version: @version,
+      description: description(),
+      package: package(),
       elixir: "~> 1.10",
+      docs: docs(),
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       aliases: [format: [&format_c/1, "format"]],
@@ -23,10 +29,42 @@ defmodule CairoEx.MixProject do
     ]
   end
 
+  defp description do
+    "Cairo wrapper for Elixir"
+  end
+
+  defp package do
+    %{
+      files: [
+        "lib",
+        "test",
+        "mix.exs",
+        "Makefile",
+        "README.md",
+        "src/*.[ch]",
+        "src/api/*.[ch]",
+        "LICENSE",
+        "CHANGELOG.md"
+      ],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
+    }
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:elixir_make, "~> 0.6", runtime: false}
+      {:elixir_make, "~> 0.6", runtime: false},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      extras: ["README.md"],
+      main: "readme",
+      source_ref: "v#{@version}",
+      source_url: @source_url
     ]
   end
 
